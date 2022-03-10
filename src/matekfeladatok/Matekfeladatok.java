550807a55dbbd3128e3cd7914bb7a1e8ee5bdb91
szf1_b <szf1_b@Tanulo-202-14.oktatas.local>
2022-03-10 15:07:22 +0100

    Atlagszamitas

--- src/matekfeladatok/Matekfeladatok.java
+++ src/matekfeladatok/Matekfeladatok.java
@@ -4,14 +4,21 @@ import java.util.Scanner;
 
 public class Matekfeladatok
 {
+    
     Scanner sc =new Scanner(System.in);
     
     public static int type_choice()
     {
+    
+        
+        
+            
+        
+
         
     }
     
-    public static int excercieses(int type)
+    public static int excercises(int type)
     {
         
     }
@@ -20,13 +27,27 @@ public class Matekfeladatok
     {
         
     }
+    // Metódus ami kiszámolja a számnak az átlagát
+    public static float results(int... stats)  {
+        int  sum = 0;
+        for (int f :stats) {
+            sum+= f;
+        }
+        return sum / stats.length;
+    }   
+    
+    
+    
+    
+    
+    
     
     public static void main(String[] args)
     {
         int type = type_choice();
-        int results = excercieses(type);
+        int results = excercises(type);
         stats(results);
-        
+        System.out.println("Átlagod száma:"+results);
     }
     
 }
