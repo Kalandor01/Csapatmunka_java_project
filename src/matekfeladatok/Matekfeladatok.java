b844832277500b898d16bc6fb72a06f925fc6ffd
Kalandor01 <rohovszkyakoska@gmail.com>
2022-03-10 14:09:51 +0100

    alapállapot

---
+++ src/matekfeladatok/Matekfeladatok.java
@@ -0,0 +1,32 @@
+package matekfeladatok;
+
+import java.util.Scanner;
+
+public class Matekfeladatok
+{
+    Scanner sc =new Scanner(System.in);
+    
+    public static int type_choice()
+    {
+        
+    }
+    
+    public static int excercieses(int type)
+    {
+        
+    }
+    
+    public static void stats(int results)
+    {
+        
+    }
+    
+    public static void main(String[] args)
+    {
+        int type = type_choice();
+        int results = excercieses(type);
+        stats(results);
+        
+    }
+    
+}
